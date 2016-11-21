bac6634fc2861bccad7e999471ab9eaaf06d5189
Alexandre Faucher <allemagne7@hotmail.fr>
2016-11-21 10:29:50 +0100

    Update Matlab / Octave code

--- haar.m
+++ haar.m
@@ -4,10 +4,10 @@ clc;
 
 pkg load image;
 
-n = 6; %1 ~ 6
+n = 1; %1 ~ 6
 
-imgRgb = imread('res/lena.ppm');
-%imgRgb = imread('res/peppers.ppm');
+%imgRgb = imread('res/lena.ppm');
+imgRgb = imread('res/peppers.ppm');
 %imgRgb = imread('res/trees.ppm');
 [h w c] = size(imgRgb);
 img = rgb2ycbcr(imgRgb);
@@ -37,10 +37,15 @@ for k=1:n
         imgRes(i+h/(2^k),j,cc) = X - Y + 128;
         imgRes(i+h/(2^k),j+w/(2^k),cc) = K - L + 128;
 
+        %quantified(i,j,cc) = imgRes(i,j,cc);
+        %quantified(i,j+w/(2^k),cc) = imgRes(i,j+w/(2^k),cc)-128 / (q/2);
+        %quantified(i+h/(2^k),j,cc) = imgRes(i+h/(2^k),j,cc)-128 / (q/2);
+        %quantified(i+h/(2^k),j+w/(2^k),cc) = imgRes(i+h/(2^k),j+w/(2^k),cc)-128 / q;
+
         quantified(i,j,cc) = imgRes(i,j,cc);
-        quantified(i,j+w/(2^k),cc) = imgRes(i,j+w/(2^k),cc)-128 / (q/2);
-        quantified(i+h/(2^k),j,cc) = imgRes(i+h/(2^k),j,cc)-128 / (q/2);
-        quantified(i+h/(2^k),j+w/(2^k),cc) = imgRes(i+h/(2^k),j+w/(2^k),cc)-128 / q;
+        quantified(i,j+w/(2^k),cc) = imgRes(i,j+w/(2^k),cc) / (q/2);
+        quantified(i+h/(2^k),j,cc) = imgRes(i+h/(2^k),j,cc) / (q/2);
+        quantified(i+h/(2^k),j+w/(2^k),cc) = imgRes(i+h/(2^k),j+w/(2^k),cc) / q;
 
         qs(i,j,cc) = 1;
         qs(i,j+w/(2^k),cc) = q/2;
@@ -65,21 +70,21 @@ figure(1);
 imshow(imgRgb);
 figure(2);
 imshow(imgRes(:,:,1));
-figure(3);
-imshow(imgRes(:,:,2));
-figure(4);
-imshow(imgRes(:,:,3));
+%figure(3);
+%imshow(imgRes(:,:,2));
+%figure(4);
+%imshow(imgRes(:,:,3));
 
 %%%%% Quantification %%%%%
 
 quantified = floor(quantified);
 %colormap jet;
-figure(5)
-imagesc(quantified(:,:,1));
-figure(6)
-imagesc(quantified(:,:,2));
-figure(7)
-imagesc(quantified(:,:,3));
+%figure(5)
+%imagesc(quantified(:,:,1));
+%figure(6)
+%imagesc(quantified(:,:,2));
+%figure(7)
+%imagesc(quantified(:,:,3));
 
 max(max(max(quantified)))
 min(min(min(quantified)))
@@ -88,6 +93,8 @@ min(min(min(quantified)))
 
 imgRecTmp = zeros(h,w,c);
 imgRec = quantified.*qs;
+figure(40);
+imshow(ycbcr2rgb(uint8(quantified)));
 
 %TODO
 for k=n:-1:1
@@ -107,12 +114,23 @@ for k=n:-1:1
 %        imgRes(i+h/(2^k),j,cc) = X - Y + 128;
 %        imgRes(i+h/(2^k),j+w/(2^k),cc) = K - L + 128;
         imgRecTmp(2*i-1,2*j-1,cc) = imgRec(i,j,cc);
-        imgRecTmp(2*i-1,2*j,cc) = imgRec(i,j,cc) + imgRec(i,j+w/(2^k),cc);
-        %imgRecTmp(2*i-1,2*j,cc) = imgRec(i,j+w/(2^k),cc) + 128;
-        imgRecTmp(2*i,2*j-1,cc) = imgRec(i,j,cc) + imgRec(i+h/(2^k),j,cc);
-        %imgRecTmp(2*i,2*j-1,cc) = imgRec(i+h/(2^k),j,cc) + 128;
-        imgRecTmp(2*i,2*j,cc) = imgRec(i,j,cc) + imgRec(i+h/(2^k),j+w/(2^k),cc);
-        %imgRecTmp(2*i,2*j,cc) = imgRec(i+h/(2^k),j+w/(2^k),cc) + 128;
+
+        %imgRecTmp(2*i-1,2*j,cc) = imgRec(i,j,cc) + imgRec(i,j+w/(2^k),cc);
+        imgRecTmp(2*i-1,2*j,cc) = (imgRec(i,j,cc) + imgRec(i,j+w/(2^k),cc))/2;
+        %imgRecTmp(2*i-1,2*j,cc) = imgRec(i,j+w/(2^k),cc);
+        %imgRec(i,j+w/(2^k),cc)
+        %%imgRecTmp(2*i-1,2*j,cc) = imgRec(i,j+w/(2^k),cc) + 128;
+
+        %imgRecTmp(2*i,2*j-1,cc) = imgRec(i,j,cc) + imgRec(i+h/(2^k),j,cc);
+        imgRecTmp(2*i,2*j-1,cc) = (imgRec(i,j,cc) + imgRec(i+h/(2^k),j,cc))/2;
+        %imgRecTmp(2*i,2*j-1,cc) = imgRec(i+h/(2^k),j,cc);
+        %%imgRecTmp(2*i,2*j-1,cc) = imgRec(i+h/(2^k),j,cc) + 128;
+
+        %imgRecTmp(2*i,2*j,cc) = imgRec(i,j,cc) + imgRec(i+h/(2^k),j+w/(2^k),cc);
+        imgRecTmp(2*i,2*j,cc) = (imgRec(i,j,cc) + imgRec(i+h/(2^k),j+w/(2^k),cc))/2;
+        %imgRecTmp(2*i,2*j,cc) = imgRec(i+h/(2^k),j+w/(2^k),cc);
+        %%imgRecTmp(2*i,2*j,cc) = imgRec(i+h/(2^k),j+w/(2^k),cc) + 128;
+
       end
     end
  end
@@ -123,8 +141,13 @@ imgRec = uint8(imgRec);
 imgRec = ycbcr2rgb(imgRec);
 figure(8);
 imshow(imgRec);
+imwrite(imgRec, 'imgRec.ppm');
+imwrite(imgRec, 'imgRec.png');
 
 %%%%% bpp/PSNR plot %%%%%
 
 %entropy of images
 %psnr(res, imgRgb)
+PSNR = psnr(imgRgb, imgRec)
+zero = uint8(zeros(h,w,c));
+PSNRzero = psnr(imgRgb, zero)
